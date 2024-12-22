2ad0323224e8d81881ae205d1df3b0096aa1b4a4
Prince Addai Desmond <166236393+bubbs-sh@users.noreply.github.com>
2024-12-22 02:52:41 +0000

    hmmm

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def dependencies(env):
             subprocess.run(["apt", "install","aria2", "-y"], check=True)
             subprocess.run(["pkg", "update", "-y"], check=True)
             subprocess.run(["apt", "install", "ruby", "python3", "curl","-y"], check=True)
-            subprocess.run(["pkg", "install", "libxml2", "libxslt" "-y"]) #these 2 are needed for lxml
+            subprocess.run(["apt", "install", "libxml2", "libxslt"]) #these 2 are needed for lxml
             subprocess.run(["gem", "install", "lolcat"], check=True) # For cololized text effect
             subprocess.run(["pip3", "install", "requests", "beautifulsoup4", "cython"], check=True) # cython isfor lxml also
             print(f"\n{bold}lxml may take a while to build.\nPlease don't interrupt.{reset}")
